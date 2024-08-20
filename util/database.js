4cb28fdb5f77c9a9faef7335284c89971f60717c
negha <neghashree96@gmail.com>
2024-08-20 22:25:37 +0530

    without sequel

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const pool=mysql.createPool({
     host:'localhost',
     user:'root',
     database:'node-complete',
-    passwort:'root'
+  
 });
 
-module.exports=pool.promise();
+module.exports = pool.promise();
