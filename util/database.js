2a556545bc78ab861c0a588fe750a1d205a378bb
negha <neghashree96@gmail.com>
2024-08-24 21:28:59 +0530

    new withsequel

--- util/database.js
+++ util/database.js
@@ -1,10 +1,12 @@
-const mysql=require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool=mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'node-complete',
-  
+
+
+const sequelize = new Sequelize('node-complete','root','root',{
+    host :"localhost",
+     dialect:"mysql",
+    port: 3305,
+
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
